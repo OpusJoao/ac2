177aa181541028351b3b426ffd2d47f5160cb478
JoãoFerreiraNewWay <joao.ferreira@gruponewway.com.br>
2022-10-12 01:11:19 -0300

    feat: create config file for firebase

---
+++ src/database/firebase/config.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBm9YX5IWjTRynuMMjBUsvDp0SqSAyAOFs",
+  authDomain: "ac2-mobile-8900a.firebaseapp.com",
+  projectId: "ac2-mobile-8900a",
+  storageBucket: "ac2-mobile-8900a.appspot.com",
+  messagingSenderId: "11883074207",
+  appId: "1:11883074207:web:617cd481de16187d68c8aa",
+  measurementId: "G-FDJLDD7EE0"
+}
